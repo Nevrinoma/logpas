4208d13df80878e180f44ba7deca69397578bef9
opilane <opilane@HP-CZC6297B9X>
2022-01-13 14:46:14 +0200

    half ready

--- login-pass/login pass/login_pass.py
+++ login-pass/login pass/login_pass.py
@@ -3,31 +3,17 @@ stored_login = ['adin', 'bbob2', 'tree']
 stored_password = ['pop2', 'bimbus', 'central']
 
 run = True
-
+print(stored_login)
+print(stored_password)
 
 while run:
     print("Hello, what u want to do?\n1 >>>Sign Up\n2 >>>Sign In\n3 >>> Quit")
     choise = input("Choise >>> ")
     if choise == "1":
-
-
-    
-    
-    
-    #print(' --- ')
-    #print('Login: ')
-    #for l in stored_login:
-    #    print(l)
-    #print('\nPassword: ')
-    #for p in stored_password:
-    #    print(p)
-    #u = input('\nu: ')
-    #p = input('p: ')
-    #i = input('i: ')
-    
-    #if i == 'quit':
-    #    run = False
-    #elif i == 'sign in':
-    #    print(signIn(u,p,stored_login,stored_password))
-    #elif i == 'sign up':
-    #    signUp(u, p,stored_login,stored_password)
+        signUp(stored_login,stored_password)
+    elif choise == "2":
+        signIn(stored_login,stored_password)
+    elif choise == "3":
+        run = False
+    else:
+        print("Try again, something wrong.")
--- login-pass/login pass/module1.py
+++ login-pass/login pass/module1.py
@@ -5,108 +5,81 @@ def signUp(stored_login,stored_password):
         newlogin = input("Write ur login >>> ")
         if newlogin not in stored_login:
             stored_login.append(newlogin)
+            print(stored_login)
+            print(stored_password)
             break
         else:
             print("This login is already taken, try again another one!")
     while True:
         print("You want to generate your password or write by yourself?")
         paschoice=input("1 >>> Generate\n2 >>> Wryte by myself\n")
-        if paschoice == 1:
+        if paschoice == "1":
             str0=".,:;!_*-+()/#¤%&"
             str1 = '0123456789'
             str2 = 'qwertyuiopasdfghjklzxcvbnm'
             str3 = str2.upper()
             str4 = str0+str1+str2+str3
             ls = list(str4)
-            random.shuffle(ls)
-            newpassword = ''.join([random.choice(ls) for x in range(12)])
+            shuffle(ls)
+            newpassword = ''.join([choice(ls) for x in range(12)])
             stored_password.append(newpassword)
+            print(stored_login)
+            print(stored_password)
             break
-        elif paschoice == 2:
+        elif paschoice == "2":
            while True:
                print("Your password must be at least 6 characters and include:uppercase and lowercase letters, numbers, and signs(.,:;!_*-+()/#¤%&)")
-                newpassword = input("")
-                numbers=0
-                upper=0
-                loww=0
-                specs=0
-                for i in newpassword:
-                    if i.isupper():
-                        upper+=1
-                    elif i.islower():
+               newpassword = input("")
+               numbers=0
+               upper=0
+               loww=0
+               specs=0
+               for i in newpassword:
+                   if i.isupper():
+                       upper+=1
+                   elif i.islower():
                         loww+=1
-                    elif i.isdigit():
+                   elif i.isdigit():
                         numbers+=1
-                    else:
+                   else:
                         specs+=1
-                if len(newpassword)<=5:
+               if len(newpassword)<=5:
                     print("Try again!")
                     continue
-                elif numbers==0:
+               elif numbers==0:
                     print("Try again!")
                     continue
-                elif upper==0:
+               elif upper==0:
                     print("Try again!")
                     continue
-                elif loww==0:
+               elif loww==0:
                     print("Try again!")
                     continue
-                elif specs==0:
+               elif specs==0:
                     print("Try again!")
                     continue
-                else:
+               else:
                     stored_password.append(newpassword)
+                    print(stored_login)
+                    print(stored_password)
                     break
 
 
 
+def signIn(stored_login,stored_password):
 
+    passwordtry = 0
+    login = input("Enter your login >>> ")
+    while True:
+        password = input("Enter your password >>> ")
+        if password not in stored_password:
+            passwordtry +=1
+            print(f"Wrong password, you have {3-passwordtry} attemps more!")
+        if passwordtry == 3:
+            print("No more attemps u have! t_t")
+            break
 
+        if login in stored_login and password in stored_password and stored_login.index(login) == stored_password.index(password):
+            print("Welcome back!")
+            break
 
-
-
-
-
-
-
-
-
-
-#def matchUserIdx(_login,stored_login):
-#    for l in stored_login:
-#        if l == _login:
-#            return stored_login.index(_login)
-#    return None
-    
-#def signIn(_login, _password,stored_login,stored_password):
-#    u_idx = matchUserIdx(_login,stored_login)
-#    if u_idx != None:
-#        if _login != stored_login[u_idx]:
-#            return False
-#        if _password != stored_password[u_idx]:
-#            return False
-#        return True
-#    return False
-    
-#def signUp(_login, _password,stored_login,stored_password):
-#    stored_login.append(_login)
-#    stored_password.append(_password)
-
-#def rndpassword():
-#    str0=".,:;!_*-+()/#¤%&"
-#    str1 = '0123456789'
-#    str2 = 'qwertyuiopasdfghjklzxcvbnm'
-#    str3 = str2.upper()
-#    print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
-#    str4 = str0+str1+str2+str3
-#    print(str4)
-#    ls = list(str4)
-#    print(ls)
-#    random.shuffle(ls)
-#    print(ls)
-#    # Извлекаем из списка 12 произвольных значений
-#    psword = ''.join([random.choice(ls) for x in range(12)])
-#    # Пароль готов
-#    print(psword)
-
-#def check():
